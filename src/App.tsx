2ae8b5e77f7335ea45d0a26cd6d853cea122aee7
Ivan Koleda <ikoleda@aristeksystems.com>
2020-06-12 19:20:24 +0300

    change config to work with aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SomeComponent from 'components/SomeComponent';
 
 interface AppProps {}
 
@@ -8,8 +8,9 @@ function App({}: AppProps) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <SomeComponent />
         <p>
+
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
---
+++ src/components/SomeComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SomeComponent: React.FC = () => {
+  return <div>Some Component</div>;
+};
+
+export default SomeComponent;
